4342500b53135e630db77ab2e5e39c36b504fa76
Frederic Gessler <gessfred@protonmail.com>
2023-06-28 23:00:12 +0200

    Move api to specific folder

--- go.mod
+++
@@ -1,8 +0,0 @@
-module sequel.gessfred.xyz
-
-go 1.20
-
-require (
-	github.com/joho/godotenv v1.5.1 // indirect
-	github.com/lib/pq v1.10.9 // indirect
-)
--- install.sh
+++
@@ -1,2 +0,0 @@
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.19.4
-go version
