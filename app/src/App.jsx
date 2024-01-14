c7cb5916a8ddc8fff75f1917917d8db28ce6560c
Frédéric Gessler <36667834+gessfred@users.noreply.github.com>
2024-01-14 08:25:15 +0000

    add connection creation dialog

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,10 +1,117 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './pages/Home'
 
-function Signin() {
-  const [state, setState] = useState({email: '', password: ''})
+function API(url, user) {
+  const call = (verb, route, callbacks, body) => {
+    const headers = {
+      method: verb
+    }
+    if(body) {
+      headers['body'] = JSON.stringify(body)
+    }
+    let response = fetch(url+route, headers)
+    if(callbacks && callbacks.success !== undefined) {
+      response = response.then(r => r.ok && callbacks.success())
+    }
+    else if(callbacks && callbacks.json !== undefined) {
+      response = response
+        .then(r => (r.ok && r.json()) || (Promise.reject(r.text())))
+        .then(callbacks.json)
+    }
+    if(callbacks && callbacks.error !== undefined) {
+      response = response.catch(err => {
+        console.log(err)
+        callbacks.error({error: err})
+      })
+    }
+    return response
+  }
+  //const callv2 = (verb, route, return_type, body) => {}
+  const get = (route, callbacks) => call('GET', route, callbacks)
+  const post = (route, body, callbacks) => call('POST', route, callbacks, body)
+  return {
+    get: get,
+    post: post,
+    auth: {
+      sendOTP: (user_email, onSuccess) => {
+        console.log('/login/otp?user_email='+user_email)
+        get('/login/otp?user_email='+user_email, {success: onSuccess})
+      },
+      getToken: (user_email, otp, onSuccess, onError) => {
+        post('/login', {otp: otp, user_email: user_email}, {
+          json: (token) => onSuccess(token),
+          error: (err) => onError(err)
+        })
+      },
+      authenticated: user !== undefined 
+    },
+    userdata: {
+      datasources: {
+        read: (callback) => {
+          console.log(user)
+          get("/datasources?owner="+user.user_email, {
+            json: callback
+          })
+        },
+        write: (datasource) => post("/datasources", Object.assign({}, datasource, {owner: user.user_email}))
+      },
+      notebooks: {
+        read: (callback) => {
+          get("/notebooks?owner="+user.user_email, {
+            json: callback
+          })
+        },
+        write: (nb) => post("/notebooks", Object.assign({}, nb, {owner: user.user_email}))
+      }
+    },
+    sql: {}
+  }
+}
+
+/*function Login({show, onLogin, api}) {
+  const [state, setState] = useState({user_email: '', otp: '', stage: 'init'})
+  console.log(state)
+  if(!show) return <span></span>
+  if(state.stage === 'init')
+    return (
+      <CenterCard >
+        <LabelInput 
+          label="Email"
+          value={state.user_email}
+          onChange={email => setState(prev => Object.assign({}, prev, {user_email: email}))} 
+        />
+        <Button onClick={() => {
+          api.auth.sendOTP(state.user_email, () => console.log('ok'))
+          setState(prev => Object.assign({}, prev, {stage: 'challenge'}))
+        }}>
+          Get code
+        </Button>
+      </CenterCard>
+    )
+  return (
+    <CenterCard >
+      <LabelInput 
+        label="Code"
+        value={state.otp}
+        onChange={code => setState(prev => Object.assign({}, prev, {otp: code}))}
+      />
+      <Button onClick={() => api.auth.getToken(state.user_email, state.otp, onLogin, console.warn)}>
+        Login
+      </Button>
+    </CenterCard>
+  )
+}*/
+
+
+
+function Signin({api, show, onLogin}) {
+  const [state, setState] = useState({user_email: '', otp: '', stage: 'init'})
+  console.log(state)
+  if(!show) return <span></span>
+  if(state.stage === 'init')
   return (
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -19,7 +126,6 @@ function Signin() {
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6" action="#" method="POST">
             <div>
               <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
                 Email address
@@ -32,64 +138,110 @@ function Signin() {
                   autoComplete="email"
                   required
 
-                  value={state.email}
-                  onChange={(e) => setState(p => Object.assign({}, p, {email: e.target.value}))}
+                  value={state.user_email}
+                  onChange={(e) => setState(p => Object.assign({}, p, {user_email: e.target.value}))}
 
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
-
-            <div>
-              <div className="flex items-center justify-between">
-                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                  Password
-                </label>
-                <div className="text-sm">
-                  <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                    Forgot password?
-                  </a>
-                </div>
-              </div>
-              <div className="mt-2">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
             <div>
               <button
-                onClick={() => console.log('ok', state)}
+                onClick={() => {
+                  api.auth.sendOTP(state.user_email, () => console.log('ok'))
+                  setState(prev => Object.assign({}, prev, {stage: 'challenge'}))
+                }}
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Get OTP
+              </button>
+            </div>
+
+
+        </div>
+      </div>
+  )
+  return (
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            className="mx-auto h-10 w-auto"
+            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            alt="Your Company"
+          />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Sign in to your account
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                OTP
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+
+                  value={state.otp}
+                  onChange={e => setState(prev => Object.assign({}, prev, {otp: e.target.value}))}
+
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div>
+              <button
+                onClick={() => api.auth.getToken(state.user_email, state.otp, onLogin, console.warn)}
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Sign in
               </button>
             </div>
-          </form>
 
-          <p className="mt-10 text-center text-sm text-gray-500">
-            Not a member?{' '}
-            <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-              Start a 14 day free trial
-            </a>
-          </p>
+
         </div>
       </div>
   )
 }
 
+/*function Home({show}) {
+  if(!show) return <span></span>
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  )
+}*/
+
 function App() {
   const [count, setCount] = useState(0)
-
+  const [state, setState] = useState({pageid: 'login'})
+  const api = API('https://sequel.gessfred.xyz/api', state.user)
+  const setStateProperty = property => setState(prev => Object.assign({}, prev, property))
+  useEffect(() => {
+    const cachedUser = localStorage.getItem("user")
+    if(cachedUser) {
+      setStateProperty({user: JSON.parse(cachedUser), pageid: 'main'})
+    }
+  }, [])
   return (
     <>
-      <Signin />
+      <Signin 
+        show={state.pageid === 'login'} 
+        api={api}
+        onLogin={user => {
+          localStorage.setItem('user', JSON.stringify(user))
+          setState({user: user, pageid: 'main'})
+        }}
+      />
+      <Home show={state.pageid === 'main'} 
+        
+      />
     </>
   )
 }
---
+++ app/src/pages/ConnectionEditor.jsx
@@ -0,0 +1,139 @@
+import { Fragment, useRef, useState } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+import { ExclamationTriangleIcon } from '@heroicons/react/24/outline'
+import Dropdown from '../components/Dropdown'
+//import uniqid from 'uniqid'
+
+const engines = [
+  {
+    id: 1,
+    name: 'PostgresSQL',
+    avatar:
+      'https://wiki.postgresql.org/images/3/30/PostgreSQL_logo.3colors.120x120.png',
+  },
+  {
+    id: 2,
+    name: 'Arlene Mccoy',
+    avatar:
+      'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    id: 3,
+    name: 'Devon Webb',
+    avatar:
+      'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80',
+  },
+  {
+    id: 4,
+    name: 'Tom Cook',
+    avatar:
+      'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  },
+  {
+    id: 5,
+    name: 'Tanya Fox',
+    avatar:
+      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  }
+]
+
+
+export function Input({name, value, setValue}) {
+    const hint = false
+    return (
+      <div>
+        <label htmlFor="price" className="block text-sm font-medium leading-6 text-gray-900">
+          {name}
+        </label>
+        <div className="relative mt-2 rounded-md shadow-sm">
+          {hint && <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+            <span className="text-gray-500 sm:text-sm">$</span>
+          </div>}
+          <input
+            type="text"
+            name="price"
+            id="price"
+            className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            value={value}
+            onChange={e => setValue(e.target.value)}
+          />
+        </div>
+      </div>
+    )
+}
+  
+export default function ConnectionEditor({api, show, onHide}) {
+  const [open, setOpen] = useState(true)
+  const [connection, setConnection] = useState({engine: engines[0]})
+  const cancelButtonRef = useRef(null)
+  const setConnectionProperty = property => value => setConnection(prev => Object.assign({}, prev, {[property]: value}))
+
+  return (
+    <Transition.Root show={open} as={Fragment}>
+      <Dialog as="div" className="relative z-10" initialFocus={cancelButtonRef} onClose={setOpen}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                  <div className="sm:flex sm:items-start">
+                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                      <ExclamationTriangleIcon className="h-6 w-6 text-red-600" aria-hidden="true" />
+                    </div>
+                    <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                      <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900">
+                        New connection
+                      </Dialog.Title>
+                      <div className="mt-2">
+                        <Dropdown name="Engine" items={engines} selected={connection.engine} setSelected={setConnectionProperty('engine')} />
+                        <Input name="Name" value={connection.name} setValue={setConnectionProperty('name')} />
+                        <Input name="Connection String" value={connection.connection_string} setValue={setConnectionProperty('connection_string')} />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                  <button
+                    type="button"
+                    className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                    onClick={() => setOpen(false)}
+                    ref={cancelButtonRef}
+                  >
+                    Create
+                  </button>
+                  <button
+                    type="button"
+                    className="inline-flex w-full justify-center rounded-md bg-slate-200 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+                    onClick={() => setOpen(false)}
+                  >
+                    Cancel
+                  </button>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
+  )
+}
